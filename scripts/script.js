5e6ee00622eb16558ef9021527e12ed13096c7d7
Štěpán Bakaj <74252828+bakajstep@users.noreply.github.com>
2021-04-22 17:11:59 +0200

    Initial commit

---
+++ scripts/script.js
@@ -0,0 +1,11 @@
+//responzivni nav
+
+function responseNav() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "nav") {
+    x.className += " responsive";
+  } else {
+    x.className = "nav";
+  }
+}
+
